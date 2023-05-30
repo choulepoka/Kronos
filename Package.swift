8d74f69fd5f3fdc163681fc9a14f800d2c2e82fb
Claude Houle <claudehoule@icloud.com>
2023-05-30 16:06:41 -0400

    Update SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,8 +1,16 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "Kronos",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v14),
+        .macCatalyst(.v14),
+        .macOS(.v12),
+        .tvOS(.v14),
+        .watchOS(.v9)
+    ],
     products: [
         .library(name: "Kronos", targets: ["Kronos"]),
     ],
